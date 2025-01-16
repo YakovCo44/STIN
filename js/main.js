e461c2ea593d9774c8fca9c11338d2d70402121f
YakovCo44 <yakovco44@gmail.com>
2025-01-17 00:47:00 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { getWeatherDescription } from './service.js'
-
 const CURRENCY_API_URL = 'https://open.er-api.com/v6/latest'
 const cityInput = document.getElementById('city')
 const citySuggestions = document.getElementById('citySuggestions')
