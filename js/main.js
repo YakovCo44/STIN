8207de6856697d1498dd1bc6d092c7e142a6620b
YakovCo44 <yakovco44@gmail.com>
2025-01-23 01:34:22 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ const cityInput = document.getElementById('city')
 const citySuggestions = document.getElementById('citySuggestions')
 const weatherResult = document.getElementById('weatherResult')
 
+function updateClock() {
+    const clockContainer = document.getElementById('clock-container')
+    const now = new Date()
+
+    const hours = String(now.getHours()).padStart(2, '0') 
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+    const seconds = String(now.getSeconds()).padStart(2, '0')
+
+    clockContainer.textContent = `${hours}:${minutes}:${seconds}`
+}
+
+setInterval(updateClock, 1000)
+updateClock()
+
